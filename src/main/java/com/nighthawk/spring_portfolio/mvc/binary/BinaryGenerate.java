241e9be4aed3d61615a5fde7c94fc743ca50f612
EdwinKuttappi <edwinab0007@gmail.com>
2024-02-02 19:48:52 -0800

    binary generate file

---
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/binary/BinaryGenerate.java
@@ -0,0 +1,12 @@
+package com.nighthawk.spring_portfolio.mvc.binary;
+
+import jakarta.persistence.Id;
+
+public class BinaryGenerate {
+    @Id
+    private Long binary;
+    public static String[] init() {
+        final String[]
+    }
+    private int number;
+}
