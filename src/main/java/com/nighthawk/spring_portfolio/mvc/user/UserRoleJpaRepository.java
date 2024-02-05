67a88303d2aff6f60e38f5d874b8c49ddfaab2c5
paravsalaniwal <paravsalaniwal@gmail.com>
2024-02-04 21:55:14 -0800

    delete

--- src/main/java/com/nighthawk/spring_portfolio/mvc/user/UserRoleJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nighthawk.spring_portfolio.mvc.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface  UserRoleJpaRepository extends JpaRepository<UserRole, Long> {
-    UserRole findByName(String name);
-}
