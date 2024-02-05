5c746acd6747f9aad418cd95b7dda31946a0da01
Shaurya Goel <shauryaggamer@gmail.com>
2024-02-05 12:15:03 -0800

    Filling up Many.java

--- src/main/java/com/nighthawk/spring_portfolio/mvc/person/ManyToMany/Many.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/person/ManyToMany/Many.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import jakarta.persistence.*;
+import java.util.Objects;
 
 @Data
 @NoArgsConstructor
@@ -24,7 +25,72 @@ public class Many {
     @JoinColumn(name = "class_id")
     private MyClass myClass;
 
-    // Other attributes related to the relationship can be added here
+    private String attendanceStatus;
 
-    // Constructors, getters, setters as needed
+    // Constructors
+
+    public Many(Person person, MyClass myClass, String attendanceStatus) {
+        this.person = person;
+        this.myClass = myClass;
+        this.attendanceStatus = attendanceStatus;
+    }
+
+    // Getter and setter methods for all attributes
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    public MyClass getMyClass() {
+        return myClass;
+    }
+
+    public void setMyClass(MyClass myClass) {
+        this.myClass = myClass;
+    }
+
+    public String getAttendanceStatus() {
+        return attendanceStatus;
+    }
+
+    public void setAttendanceStatus(String attendanceStatus) {
+        this.attendanceStatus = attendanceStatus;
+    }
+
+    // Other methods or business logic
+
+    public String getPersonName() {
+        return person != null ? person.getName() : null;
+    }
+
+    public String getClassName() {
+        return myClass != null ? myClass.getClassName() : null;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Many many = (Many) o;
+        return Objects.equals(id, many.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    // Add more methods as needed based on your use case
 }
