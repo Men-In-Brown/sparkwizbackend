4e57cdf29473ae51a85af484a40ccc53f981c62e
Shaurya Goel <shauryaggamer@gmail.com>
2024-02-05 12:08:18 -0800

    Setting up ManyApiController.java

--- src/main/java/com/nighthawk/spring_portfolio/mvc/person/ManyToMany/ManyApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/person/ManyToMany/ManyApiController.java
@@ -1,5 +1,32 @@
-package com.nighthawk.spring_portfolio.mvc.person.ManyToMany;
+package com.nighthawk.spring_portfolio.mvc.many;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/many")
 public class ManyApiController {
-    
+
+    @Autowired
+    private ManyJpaRepository manyRepository;
+
+    // Other dependencies and methods can be added based on your requirements
+
+    @GetMapping("/")
+    public ResponseEntity<List<Many>> getManyRecords() {
+        return new ResponseEntity<>(manyRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Many> getManyRecord(@PathVariable long id) {
+        return manyRepository.findById(id)
+                .map(record -> new ResponseEntity<>(record, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    // Other CRUD methods and endpoints can be added based on your requirements
 }
