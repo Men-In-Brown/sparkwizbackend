074209135702809c3f68f5785a8b256e38a32e13
Shaurya Goel <shauryaggamer@gmail.com>
2024-02-05 12:07:51 -0800

    Setting up Many.java

--- src/main/java/com/nighthawk/spring_portfolio/mvc/person/ManyToMany/Many.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/person/ManyToMany/Many.java
@@ -1,5 +1,30 @@
-package com.nighthawk.spring_portfolio.mvc.person.ManyToMany;
+package com.nighthawk.spring_portfolio.mvc.many;
 
+import com.nighthawk.spring_portfolio.mvc.person.Person;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Many {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "person_id")
+    private Person person;
+
+    @ManyToOne
+    @JoinColumn(name = "class_id")
+    private MyClass myClass;
+
+    // Other attributes related to the relationship can be added here
+
+    // Constructors, getters, setters as needed
 }
