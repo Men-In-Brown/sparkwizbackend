b46e7936a09709e53c03a0d5dbb34ef97bea9af3
VINERAJ <vinay.e.rajagopalan@gmail.com>
2024-02-05 22:02:00 -0800

    fixing binary code

--- src/main/java/com/nighthawk/spring_portfolio/mvc/binary/BinaryGenerate.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/binary/BinaryGenerate.java
@@ -6,13 +6,15 @@ import java.util.Random;
 public class BinaryGenerate {
     @Id
     public static double[] getBinary() {
-        Long binary;
         Random rand = new Random();
         double decimal = rand.nextDouble()*255;
         decimal = Math.floor(decimal);
-        binary = Double.doubleToLongBits(decimal);
+        String binaryString = Long.toBinaryString((long) decimal);
+        while (binaryString.length() < 8) {
+            binaryString = "0" + binaryString;
+        }
         double[] bools = new double[2];
-        bools[0] = binary;
+        bools[0] = Long.parseLong(binaryString);
         bools[1] = decimal;
         return bools;
     };
