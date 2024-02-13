90cfabf1af8d877e9bf1215cae183a0d4877490b
EdwinKuttappi <edwinab0007@gmail.com>
2024-02-13 11:26:46 -0800

    formatting into jpkes mvc

--- src/main/java/com/nighthawk/spring_portfolio/mvc/test/DataTestRoleJpaRepository.java
+++
@@ -1,7 +0,0 @@
-// package com.nighthawk.spring_portfolio.mvc.test;
-
-// import org.springframework.data.jpa.repository.JpaRepository;
-
-// public interface  DataTestRoleJpaRepository extends JpaRepository<DataTestRole, Long> {
-//     PersonRole findByName(String name);
-// }
